519d49ec3f09d13b9bddec82c13024db01f6674f
Svilen Vasilev <svilen@svilenvasilev.com>
2020-05-20 15:29:43 +0300

    initial

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Python Crash Course 2nd Edition
+
+print('Hello Python world!')
+
+message = 'Hello World!'
+print(message)
+
+name = "jOHn smItH"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}" 
+print(full_name)
